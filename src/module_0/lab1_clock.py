4dd171b1260c8659940e1ac9686444f6ac85467a
gmabry700 <gmabry700@access.alamancecc.edu>
2026-09-17 00:23:11 -0400

    update timestamp typo and offset

--- src/module_0/lab1_clock.py
+++ src/module_0/lab1_clock.py
@@ -1,8 +1,9 @@
 # Refer to this module's readme
 def main():
-    time = 174160440
+    time = 1741604400
     timezone = "US/Eastern"
-    time = time + 1567444260
+    # time = time + 1567444260
+    time = time + 300
     print(str(time) + " in " + timezone)
 
 main()
