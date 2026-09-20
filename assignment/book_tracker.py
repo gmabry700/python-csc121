9e0e56fa3739d4cef4e982d675235cc0f252fa11
gmabry700 <gmabry700@access.alamancecc.edu>
2026-09-19 22:02:21 -0400

    update add_book() to accept a list and return book dictionary

--- assignment/book_tracker.py
+++ assignment/book_tracker.py
@@ -15,19 +15,18 @@ def estimate_reading_time(pages):
     return round(float(pages/40), 1)
 
 
-def add_book():
-    """
-    This function takes in user input for title, author, and page count.
-    Create a variable called hours that calls the function estimate_reading_time
-    """
-    title = input("Book title: ").title().strip()    # get user input in title case for "Book title: "
-    author = input("Author: ").title().strip()       # get user input for "Author: "
-    pages = int(input("Page count: "))               # get user input as an int for "Page count: "
-    hours = estimate_reading_time(pages)             # call estimate_reading_time by passing in pages
+def add_book(library=[]):
+    book = {}
+    book["title"] = input("Book title: ").title().strip()    # get user input in title case for "Book title: "
+    book["author"] = input("Author: ").title().strip()       # get user input for "Author: "
+    book["pages"] = int(input("Page count: "))               # get user input as an int for "Page count: "
+    book["hours"] = estimate_reading_time(book["pages"])             # call estimate_reading_time by passing in pages
+    library.append(book)
 
     # use an f-string to print "'{title}' by {author} -- approx. {hours} to read"
     print("\nBook added:\n")
-    print(f"  '{title}' by {author} -- approx. {hours} hours to read")
+    print(f"""  '{book["title"]}' by {book["author"]} -- approx. {book["hours"]} hours to read""")
+    print(book)
 
 
 def show_menu():
@@ -44,8 +43,6 @@ def show_menu():
     return option
 
 def main():
-    dashboard()
-    add_book()
     option = ''
     while option != 'q':
         dashboard()
