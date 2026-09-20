364e6aa65e4b13d6491ac252d1a13acd2d8bb73d
gmabry700 <gmabry700@access.alamancecc.edu>
2026-09-20 00:01:25 -0400

    add menu loop, library list, show_menu and view_books functions

--- assignment/book_tracker.py
+++ assignment/book_tracker.py
@@ -20,13 +20,22 @@ def add_book(library=[]):
     book["title"] = input("Book title: ").title().strip()    # get user input in title case for "Book title: "
     book["author"] = input("Author: ").title().strip()       # get user input for "Author: "
     book["pages"] = int(input("Page count: "))               # get user input as an int for "Page count: "
-    book["hours"] = estimate_reading_time(book["pages"])             # call estimate_reading_time by passing in pages
+    book["hours"] = estimate_reading_time(book["pages"])     # call estimate_reading_time by passing in pages
     library.append(book)
 
-    # use an f-string to print "'{title}' by {author} -- approx. {hours} to read"
     print("\nBook added:\n")
     print(f"""  '{book["title"]}' by {book["author"]} -- approx. {book["hours"]} hours to read""")
-    print(book)
+
+    return book
+
+
+def view_books(library=[]):
+    if len(library) == 0:
+        print("Your library is empty. Add a book first!")
+    else:
+        for num in range(len(library)):
+            book = library[num]
+            print(f"""  {num+1}. '{book["title"]}' - {book["author"]} ({book["pages"]} pages - approx. {book["hours"]} hours to read)""")
 
 
 def show_menu():
@@ -42,11 +51,26 @@ def show_menu():
     option = input("> ").strip().lower()
     return option
 
+
 def main():
+    library = []
     option = ''
+    dashboard()
     while option != 'q':
-        dashboard()
         option = show_menu()
+
+        match option:
+            case '1':   # View books
+                view_books(library)
+            case '2':   # Add a book
+                add_book(library)
+            case 'q' | 'quit' | 'exit':
+                break
+            case _:
+                print("Sorry, that option isn't available.")
+                continue
+
+    print("Goodbye!")
 
 
 if __name__ == "__main__":
