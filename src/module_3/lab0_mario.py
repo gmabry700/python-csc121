a03889d1b43e0d2572244ff95ad4fd3ad2040092
gmabry700 <gmabry700@access.alamancecc.edu>
2026-09-22 16:19:31 -0400

    module 3 labs

--- src/module_3/lab0_mario.py
+++ src/module_3/lab0_mario.py
@@ -1,1 +1,14 @@
-# Refer to this module's readme
+
+def main():
+    height = int(input("Height: "))
+    pyramid (height)
+
+
+def pyramid(n):
+    for i in range(n):
+        # print(i, end=" ")
+        print("#" * (i + 1))
+
+
+if __name__ == "__main__":
+    main()
